2b05d7c2a026bf2511232367d04863aeb99712d2
nishantcm <cmnishantwork@gmail.com>
2024-11-15 00:37:53 +0530

    Update the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ContactForm from './components/ContactForm.jsx';
 
 let App = () => {
   return (
     <>
       <h2>Contact Form</h2>
+      <ContactForm></ContactForm>
     </>
   );
 }
