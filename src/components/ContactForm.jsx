ec88fa75c63479b9e167b1d056a7fafcb4cae761
nishantcm <cmnishantwork@gmail.com>
2024-11-15 00:28:20 +0530

    Update the ContactForm.jsx file

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,8 @@
+
+export default function Form() {
+    return (
+        <Section>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quibusdam.</p>
+        </Section>
+    );
+}
