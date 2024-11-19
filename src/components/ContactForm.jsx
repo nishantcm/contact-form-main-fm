125c5e0485422275eb2201a51c323af1a5bd6a68
nishantcm <cmnishantwork@gmail.com>
2024-11-19 23:56:41 +0530

    Update the file

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -23,6 +23,27 @@ export default function Form() {
                                             <input type="text" class="form-control" id="last-name" aria-describedby="basic-addon3"/> 
                                         </div>
                                     </div>
+                                    <div className="col-12 col-sm-12 col-md-12">
+                                        <label for="email" class="form-label" required>Email</label>
+                                        <div class="form-label mb-3">
+                                            <input type="text" class="form-control" id="email" aria-describedby="basic-addon3"/> 
+                                        </div>
+                                    </div>
+                                    <div className="col-12 col-sm-12 col-md-12">
+                                        <label for="email" class="form-label" required>Query Type</label>
+                                        <div class="form-label mb-3">
+                                            <input type="radio" class="form-label" name="btnradio" id="radio1" autocomplete="off" checked />
+                                            <label class="btn btn-outline-secondary" for="radio1">Radio 1</label> 
+                                        </div>
+                                        <div class="form-label mb-3">
+                                            <input type="radio" class="form-label" name="btnradio" id="radio1" autocomplete="off" checked />
+                                            <label class="btn btn-outline-secondary" for="radio1">Radio 1</label> 
+                                        </div>
+                                        <div class="form-label mb-3">
+                                            <label class="form-label" for="message" required>Message</label> 
+                                            <textarea type="text" class="form-control" id="message" aria-describedby="basic-addon3"></textarea> 
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
