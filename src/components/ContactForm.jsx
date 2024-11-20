1dc94435344506e217d36d538deb759b37d6d146
nishantcm <cmnishantwork@gmail.com>
2024-11-20 23:28:00 +0530

    Update the file

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -31,17 +31,27 @@ export default function Form() {
                                     </div>
                                     <div className="col-12 col-sm-12 col-md-12">
                                         <label for="email" class="form-label" required>Query Type</label>
-                                        <div class="form-label mb-3">
-                                            <input type="radio" class="form-label" name="btnradio" id="radio1" autocomplete="off" checked />
-                                            <label class="btn btn-outline-secondary" for="radio1">Radio 1</label> 
-                                        </div>
-                                        <div class="form-label mb-3">
-                                            <input type="radio" class="form-label" name="btnradio" id="radio1" autocomplete="off" checked />
-                                            <label class="btn btn-outline-secondary" for="radio1">Radio 1</label> 
+                                        <div className="row">
+                                            <div className="col-sm-12 col-md-6">
+                                                <div class="form-label mb-3">
+                                                    <input type="radio" class="form-check-input" name="btnradio" id="radio1" autocomplete="off" />
+                                                    <label class="form-check-label" for="radio1">General Enquiry</label> 
+                                                </div>
+                                            </div>
+                                            <div className="col-sm-12 col-md-6">
+                                                <div class="form-label mb-3">
+                                                    <input type="radio" class="form-check-input" name="btnradio" id="radio2" autocomplete="off" />
+                                                    <label class="form-check-label" for="radio2">Support Request</label> 
+                                                </div>
+                                            </div>
                                         </div>
                                         <div class="form-label mb-3">
                                             <label class="form-label" for="message" required>Message</label> 
                                             <textarea type="text" class="form-control" id="message" aria-describedby="basic-addon3"></textarea> 
+                                        </div>
+                                        <div>
+                                            <input type="checkbox" class="form-check-input" name="btnradio" id="checkbox" autocomplete="off" />
+                                            <label class="form-check-label" for="checkbox">Support Request</label>
                                         </div>
                                     </div>
                                 </div>
