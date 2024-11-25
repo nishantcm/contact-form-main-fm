4b4f400e0d731a014916faece33e11a86af69827
nishantcm <cmnishantwork@gmail.com>
2024-11-26 00:30:04 +0530

    Update the file

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -31,32 +31,32 @@ export default function Form() {
                                             </div>
                                         </div>
                                         <div className="col-12 col-sm-12 col-md-12">
-                                            <label for="email" className="form-label">Query Type <span className="symbolRequired">*</span></label>
+                                            <label for="radio1" className="form-label">Query Type <span className="symbolRequired">*</span></label>
                                             <div className="row">
                                                 <div className="col-sm-12 col-md-6">
                                                     <div className="form-label mb-3 border border-1 p-2 rounded-2">
-                                                        <input type="radio" className="form-check-input ms-2 me-2" id="radio1" autocomplete="off"/>
+                                                        <input type="radio" className="form-check-input ms-2 me-2" name="queryType" id="radio1" autocomplete="off"/>
                                                         <label className="form-check-label" for="radio1">General Enquiry</label> 
                                                     </div>
                                                 </div>
                                                 <div className="col-sm-12 col-md-6">
                                                     <div className="form-label mb-3 border border-1 p-2 rounded-2">
-                                                        <input type="radio" className="form-check-input ms-2 me-2" id="radio2" autocomplete="off"/>
+                                                        <input type="radio" className="form-check-input ms-2 me-2" name="queryType" id="radio2" autocomplete="off"/>
                                                         <label className="form-check-label" for="radio2">Support Request</label> 
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="form-label mb-3">
-                                                <label className="form-label" for="message">Message <span className="symbolRequired">*</span></label> 
-                                                <textarea type="text" className="form-control" id="message" aria-describedby="basic-addon3" required></textarea> 
-                                            </div>
-                                            <div className="d-flex">
-                                                <input type="checkbox" className="form-check-input me-3" id="consentCheck" required />
-                                                <label className="form-check-label" for="consentCheck">I consent to being contacted by the team <span className="symbolRequired">*</span></label>
-                                            </div>
-                                            <div className="d-grid gap-2 mt-3">
-                                                <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
-                                            </div>
+                                        </div>
+                                        <div className="form-label mb-3">
+                                            <label className="form-label" for="message">Message <span className="symbolRequired">*</span></label> 
+                                            <textarea type="text" className="form-control" id="message" aria-describedby="basic-addon3" required></textarea> 
+                                        </div>
+                                        <div className="d-flex">
+                                            <input type="checkbox" className="form-check-input me-3" id="consentCheck" required />
+                                            <label className="form-check-label" for="consentCheck">I consent to being contacted by the team <span className="symbolRequired">*</span></label>
+                                        </div>
+                                        <div className="d-grid gap-2 mt-3">
+                                            <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
                                         </div>
                                     </div>
                                 </form>
