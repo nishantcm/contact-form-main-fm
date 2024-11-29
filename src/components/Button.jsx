ff03839fb7835ea09826a7e00035204f541c5b56
nishantcm <cmnishantwork@gmail.com>
2024-11-29 23:59:42 +0530

    Create the file Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Button() {
+    return(
+        <div className="d-grid gap-2 mt-3">
+            <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
+        </div>
+    )
+}
