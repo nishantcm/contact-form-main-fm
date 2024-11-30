cebbdaee99279439ebd447cf791ea4996dc721c4
nishantcm <cmnishantwork@gmail.com>
2024-11-30 23:53:59 +0530

    Update the file

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import EmailInput from './EmailInput';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <EmailInput />
   </React.StrictMode>
 );
 
