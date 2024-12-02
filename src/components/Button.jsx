bc68ae7c2d40f8248bef7b9745513044fef3d84e
nishantcm <cmnishantwork@gmail.com>
2024-12-02 23:57:45 +0530

    Update the file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
-
+import { useFormStatus } from "react-dom";
+ 
 
 export default function Button() {
+    
     return(
         <div className="d-grid gap-2 mt-3">
             <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
         </div>
     )
+    
 }
