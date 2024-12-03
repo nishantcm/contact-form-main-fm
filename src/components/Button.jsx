409f156028df466f975916a3b4bbd529549aab75
nishantcm <cmnishantwork@gmail.com>
2024-12-04 00:17:53 +0530

    Update the file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import React, { useForm } from "react";
+import React, { useState } from "react";
  
 
 export default function Button() {
@@ -12,12 +12,19 @@ export default function Button() {
             setShowAlert(false);
           }, 3000);
         };
-    return(
+    return (
         <form onSubmit={handleSubmit}>
             <div className="d-grid gap-2 mt-3">
                 <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
             </div>
+        
+
+            {showAlert && (
+                <div>
+                    Success! Your form has been submitted.
+                </div>
+            )}
         </form>
-    )
+    );
     
-}
+};
