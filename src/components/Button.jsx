5950eb389c424ed7aa3b7dcb07726c193f10bfd9
nishantcm <cmnishantwork@gmail.com>
2024-12-05 23:21:35 +0530

    Update the file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,30 +1,22 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
  
 
 export default function Button() {
-    const [showAlert, setShowAlert] = useState(false);
+    // const [showAlert, setShowAlert] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setShowAlert(true);
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     setShowAlert(true);
 
-        setTimeout(() => {
-            setShowAlert(false);
-          }, 3000);
-        };
+    //     setTimeout(() => {
+    //         setShowAlert(false);
+    //       }, 3000);
+    //     };
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="d-grid gap-2 mt-3">
-                <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
-            </div>
-        
-
-            {showAlert && (
-                <div>
-                    Success! Your form has been submitted.
-                </div>
-            )}
-        </form>
+        <div className="d-grid gap-2 mt-3">
+            <button className="btn buttonColor text-white text-center py-2" type="submit">Submit</button>
+        </div>
+    
     );
     
 };
